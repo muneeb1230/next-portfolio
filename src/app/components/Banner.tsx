810d6fb38cadcec05390a2fdb489d512bab17295
Muneeb Malik <144794302+muneeb1230@users.noreply.github.com>
2024-11-10 14:42:12 +0500

    changes

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,29 +1,26 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 function Banner() {
   return (
-    <section  className='who_am'>
-    <div className='container'>
-     <div className='row'>
-        <div className='about'>
-         <p>Hi,  This is me </p>
-         <h1>Muneeb Ali </h1>
-         <p> I'm a passionate web developer specializing in creating dynamic and responsive websites. With a strong foundation in HTML, CSS, and JavaScript, I bring innovative ideas to life through clean and efficient code. I'm dedicated to delivering high-quality work that meets client needs and exceeds expectations.
-
-         </p>
-         <button >
-             Contact me
-         </button>
+    <section className='who_am'>
+      <div className='container'>
+        <div className='row'>
+          <div className='about'>
+            <p>Hi, This is me</p>
+            <h1>Muneeb Ali</h1>
+            <p>
+              I&apos;m a passionate web developer specializing in creating dynamic and responsive websites. With a strong foundation in HTML, CSS, and JavaScript, I bring innovative ideas to life through clean and efficient code. I&apos;m dedicated to delivering high-quality work that meets client needs and exceeds expectations.
+            </p>
+            <button>Contact me</button>
+          </div>
+          <div className='img'>
+            <img src="/muneeb.jpg" alt="" />
+          </div>
         </div>
-        <div className='img'>
-         <Image width={300} height={300} src='/muneeb.jpg' alt='muneeb.jgp' />
-        </div>
-
-     </div>
-    </div> 
-  </section>
-  )
+      </div>
+    </section>
+  );
 }
 
-export default Banner
+export default Banner;
--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,68 +1,67 @@
-import React from 'react'
-
+import React from 'react';
 import Image from 'next/image';
 
 function Contact() {
   return (
-    <section id='contact' className='contact'>
-      <div className='container'>
+    <section id="contact" className="contact">
+      <div className="container">
         <h2>Contact Me</h2>
-        <p id='para'>
-          'Let's connect and create something amazing together—reach out to start the conversation!'
+        <p id="para">
+          Let&apos;s connect and create something amazing together—reach out to start the conversation!
         </p>
 
-        <div className='row'>
-          <div className='box'>
-            <Image src='/contat 1.avif' alt='Address Icon' width={86} height={86} />
-            <p className='para'>ADDRESS</p>
+        <div className="row">
+          <div className="box">
+            <Image src="/contat 1.avif" alt="Address Icon" width={86} height={86} />
+            <p className="para">ADDRESS</p>
             <p>Karachi, Sindh</p>
           </div>
-          <div className='box'>
-            <Image src='/contat2.avif' alt='Contact Number Icon' width={86} height={86} />
-            <p className='para'>CONTACT NO</p>
+          <div className="box">
+            <Image src="/contat2.avif" alt="Contact Number Icon" width={86} height={86} />
+            <p className="para">CONTACT NO</p>
             <p>+92 317,3829361</p>
           </div>
-          <div className='box'>
-            <Image src='/contat3.avif' alt='Email Icon' width={86} height={86} />
-            <p className='para'>EMAIL ADDRESS</p>
+          <div className="box">
+            <Image src="/contat3.avif" alt="Email Icon" width={86} height={86} />
+            <p className="para">EMAIL ADDRESS</p>
             <p>muneebmalikdev@gmail.com</p>
           </div>
-          <div className='box'>
-            <a href='https://www.linkedin.com/in/muneeb-ali-2062b62b6/' target='_blank' rel='noopener noreferrer'>
-              <Image src='/contact4.avif' alt='LinkedIn Icon' width={86} height={86} />
+          <div className="box">
+            <a href="https://www.linkedin.com/in/muneeb-ali-2062b62b6/" target="_blank" rel="noopener noreferrer">
+              <Image src="/contact4.avif" alt="LinkedIn Icon" width={86} height={86} />
             </a>
-            <p className='para'>LINKEDIN</p>
+            <p className="para">LINKEDIN</p>
             <p>Muneeb Ali</p>
           </div>
         </div>
 
-        <div className='row-2'>
-          <Image src='/muneeb.jpg' id='img-contat' alt='Contact Image' width={380} height={480} />
+        <div className="row-2">
+          <Image src="/muneeb.jpg" id="img-contat" alt="Contact Image" width={380} height={480} />
 
-          <div className='form'>
-            <div className='name-fields'>
-              <div className='input-field'>
-                <label htmlFor='first-name'>First Name</label>
-                <input type='text' id='first-name' placeholder='John' />
+          <div className="form">
+            <div className="name-fields">
+              <div className="input-field">
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" id="first-name" placeholder="John" />
               </div>
 
-              <div className='input-field'>
-                <label htmlFor='last-name'>Last Name</label>
-                <input type='text' id='last-name' placeholder='Alfard' />
+              <div className="input-field">
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" id="last-name" placeholder="Alfard" />
               </div>
             </div>
 
-            <div className='input-field'>
-              <label htmlFor='email'>Email</label>
-              <input type='email' id='email' placeholder='example@mail.com' />
+            <div className="input-field">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="example@mail.com" />
             </div>
 
-            <div className='input-field'>
-              <label htmlFor='message'>Message</label>
-              <textarea id='message' placeholder='Your message...'></textarea>
+            <div className="input-field">
+              <label htmlFor="message">Message</label>
+              <textarea id="message" placeholder="Your message..."></textarea>
             </div>
 
-            <button type='submit'>Submit</button>
+            <button type="submit">Submit</button>
           </div>
         </div>
       </div>
@@ -71,4 +70,3 @@ function Contact() {
 }
 
 export default Contact;
-
